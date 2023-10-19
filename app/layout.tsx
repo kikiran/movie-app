ad05317eee8e7927e08e74d6ea4a63fe7cc713b5
Kirankumar <kiran.m79955@gmail.com>
2023-10-19 16:52:44 +0530

    app creation with nextJS

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Movie Search App',
+  description: 'Search filter and find movies',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
